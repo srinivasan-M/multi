d64222d58e266c33d5b309995d82972fe73cedf2
srinivasan-M <srinivasanksr15@gmail.com>
2017-04-18 21:10:53 +0530

    Update multi.java

--- multi.java
+++ multi.java
@@ -5,6 +5,9 @@ int n,c;
 System.out.print("Input the integer to print its multiplication table");
 Scanner in=new Scanner(System.in);
 n=in.nexiInt();
+  if(n<0)
+    System.out.print("Please input valid number");
+  else
 System.out.print("Multiplication table of "+n+" is:-");
 for(c=1;c<=10;c++)
 System.out.print(n+"*"+c+"="+(n*c));}}
